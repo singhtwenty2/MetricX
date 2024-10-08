200c0ff517d96d6cb541409276177947cc36f8df
Aryan Singh <aryansingh97713@gmail.com>
2024-10-09 00:03:29 +0530

    Added Website-Details Controller

--- AUTH_SERVICE/src/main/kotlin/example/com/controller/WebsiteController.kt
+++ AUTH_SERVICE/src/main/kotlin/example/com/controller/WebsiteController.kt
@@ -1,6 +1,8 @@
 package example.com.controller
 
-import example.com.data.dto.request.WebsiteDetailsDTO
+import example.com.data.dto.internal.InternalWebsiteDetailsDTO
+import example.com.data.dto.request.UpdateWebsiteDetailsDTO
+import example.com.data.dto.request.WebsiteDetailsCreationDTO
 import example.com.data.dto.response.MessageDTO
 import example.com.service.WebsiteService
 import io.ktor.http.HttpStatusCode
@@ -13,10 +15,10 @@ class WebsiteController(
 ) {
     suspend fun createWebsite(call: ApplicationCall) {
         val clusterId = call.request.queryParameters["cId"]
-        val request = call.receive<WebsiteDetailsDTO>()
+        val request = call.receive<WebsiteDetailsCreationDTO>()
         clusterId?.let {
             val isWebsiteDetailsCreated = websiteService.createWebsite(
-                details = WebsiteDetailsDTO(
+                details = InternalWebsiteDetailsDTO(
                     clusterId = it,
                     websiteUrl = request.websiteUrl,
                     apiKey = request.apiKey,
@@ -62,14 +64,82 @@ class WebsiteController(
     }
 
     suspend fun getWebsite(call: ApplicationCall) {
-        websiteService.getWebsite()
+        val clusterId = call.request.queryParameters["cId"]
+        val websiteId = call.request.queryParameters["wId"]
+        clusterId?.let {cId ->
+            websiteId?.let { wId ->
+                val result = websiteService.getWebsite(cId, wId)
+                return@let call.respond(
+                    status = HttpStatusCode.OK,
+                    message = result
+                )
+            } ?: call.respond(
+                status = HttpStatusCode.BadRequest,
+                message = MessageDTO(errorMessage = "Website Id Not Found")
+            )
+        } ?: call.respond(
+            status = HttpStatusCode.BadRequest,
+            message = MessageDTO(errorMessage = "Cluster Id Not Found")
+        )
     }
 
     suspend fun updateWebsite(call: ApplicationCall) {
-        websiteService.updateWebsite()
+        val cId = call.request.queryParameters["cId"]
+        val wId = call.request.queryParameters["wId"]
+        val request = call.receive<UpdateWebsiteDetailsDTO>()
+        cId?.let { clusterId ->
+            wId?.let { websiteId ->
+                val isWebsiteUpdated = websiteService.updateWebsite(
+                    cId = clusterId,
+                    wId = websiteId,
+                    updateWebsiteDetailsDTO = request
+                )
+                if(!isWebsiteUpdated) {
+                    return@let call.respond(
+                        status = HttpStatusCode.Conflict,
+                        message = MessageDTO(errorMessage = "Failed To Update Website")
+                    )
+                }
+                return@let call.respond(
+                    status = HttpStatusCode.OK,
+                    message = MessageDTO(successMessage = "Website Updated Successfully")
+                )
+            } ?: call.respond(
+                status = HttpStatusCode.BadRequest,
+                message = MessageDTO(errorMessage = "Website Id Not Found")
+            )
+        } ?: call.respond(
+            status = HttpStatusCode.BadRequest,
+            message = MessageDTO(errorMessage = "Cluster Id Not Found")
+        )
     }
 
-    suspend fun deleteWebsite() {
-        websiteService.deleteWebsite()
+    suspend fun deleteWebsite(call: ApplicationCall) {
+        val cId = call.request.queryParameters["cId"]
+        val wId = call.request.queryParameters["wId"]
+        cId?.let { clusterId ->
+            wId?.let { websiteId ->
+                val isWebsiteDeleted = websiteService.deleteWebsite(
+                    cId = clusterId,
+                    wId = websiteId
+                )
+                if(!isWebsiteDeleted) {
+                    return@let call.respond(
+                        status = HttpStatusCode.Conflict,
+                        message = MessageDTO(errorMessage = "Failed To Delete Website")
+                    )
+                }
+                return@let call.respond(
+                    status = HttpStatusCode.OK,
+                    message = MessageDTO(successMessage = "Website Deleted Successfully")
+                )
+            } ?: call.respond(
+                status = HttpStatusCode.BadRequest,
+                message = MessageDTO(errorMessage = "Website Id Not Found")
+            )
+        } ?: call.respond(
+            status = HttpStatusCode.BadRequest,
+            message = MessageDTO(errorMessage = "Cluster Id Not Found")
+        )
     }
 }
--- AUTH_SERVICE/src/main/kotlin/example/com/data/repository/dao/WebsiteDAO.kt
+++ AUTH_SERVICE/src/main/kotlin/example/com/data/repository/dao/WebsiteDAO.kt
@@ -1,9 +1,10 @@
 package example.com.data.repository.dao
 
-import example.com.data.dto.internal.InternalWebsiteDetailsCreateDTO
+import example.com.data.dto.request.UpdateWebsiteDetailsDTO
 import example.com.data.dto.response.WebsiteDetailResponseDTO
 import example.com.data.repository.entity.Websites
 import example.com.util.RecordCreationErrorHandler
+import org.jetbrains.exposed.sql.SortOrder
 import org.jetbrains.exposed.sql.and
 import org.jetbrains.exposed.sql.deleteWhere
 import org.jetbrains.exposed.sql.insert
@@ -17,24 +18,27 @@ object WebsiteDAO {
 
     fun createWebsite(
         clusterId: UUID,
-        internalWebsiteDetailsCreateDTO: InternalWebsiteDetailsCreateDTO
+        currentTime: String,
+        updateWebsiteDetailsDTO: UpdateWebsiteDetailsDTO
     ): RecordCreationErrorHandler {
         return transaction {
             val record = Websites.selectAll().where {
-                Websites.websiteUrl eq internalWebsiteDetailsCreateDTO.websiteUrl
+                Websites.websiteUrl eq updateWebsiteDetailsDTO.websiteUrl
             }.singleOrNull()
             if(record != null) {
                 RecordCreationErrorHandler.AlreadyExists(
-                    errorMessage = "Website with URL ${internalWebsiteDetailsCreateDTO.websiteUrl} already exists",
+                    errorMessage = "Website with URL ${updateWebsiteDetailsDTO.websiteUrl} already exists",
                 )
             } else {
                 Websites.insert {
                     it[Websites.clusterId] = clusterId
-                    it[websiteUrl] = internalWebsiteDetailsCreateDTO.websiteUrl
-                    it[apiKey] = internalWebsiteDetailsCreateDTO.apiKey
-                    it[authToken] = internalWebsiteDetailsCreateDTO.authToken
-                    it[headers] = internalWebsiteDetailsCreateDTO.headers
-                    it[maxRetries] = internalWebsiteDetailsCreateDTO.maxRetries
+                    it[websiteUrl] = updateWebsiteDetailsDTO.websiteUrl
+                    it[apiKey] = updateWebsiteDetailsDTO.apiKey
+                    it[authToken] = updateWebsiteDetailsDTO.authToken
+                    it[headers] = updateWebsiteDetailsDTO.headers
+                    it[maxRetries] = updateWebsiteDetailsDTO.maxRetries
+                    it[createdAt] = currentTime
+                    it[updatedAt] = currentTime
                 }
                 RecordCreationErrorHandler.Success(
                     successMessage = "Website created successfully",
@@ -49,7 +53,7 @@ object WebsiteDAO {
         return transaction {
             Websites.selectAll().where {
                 Websites.clusterId eq clusterId
-            }.orderBy(Websites.createdAt).map {
+            }.orderBy(Websites.updatedAt, SortOrder.DESC).map {
                 WebsiteDetailResponseDTO(
                     clusterId = it[Websites.clusterId].toString(),
                     websiteId = it[Websites.websiteId].toString(),
@@ -91,7 +95,8 @@ object WebsiteDAO {
     fun updateWebsite(
         clusterId: UUID,
         websiteId: UUID,
-        internalWebsiteDetailsCreateDTO: InternalWebsiteDetailsCreateDTO
+        currentTime: String,
+        updateWebsiteDetailsDTO: UpdateWebsiteDetailsDTO
     ): RecordCreationErrorHandler {
         return transaction {
             val record = Websites.selectAll().where {
@@ -105,11 +110,12 @@ object WebsiteDAO {
                 Websites.update({
                     (Websites.clusterId eq clusterId) and (Websites.websiteId eq websiteId)
                 }) {
-                    it[websiteUrl] = internalWebsiteDetailsCreateDTO.websiteUrl
-                    it[apiKey] = internalWebsiteDetailsCreateDTO.apiKey
-                    it[authToken] = internalWebsiteDetailsCreateDTO.authToken
-                    it[headers] = internalWebsiteDetailsCreateDTO.headers
-                    it[maxRetries] = internalWebsiteDetailsCreateDTO.maxRetries
+                    it[websiteUrl] = updateWebsiteDetailsDTO.websiteUrl
+                    it[apiKey] = updateWebsiteDetailsDTO.apiKey
+                    it[authToken] = updateWebsiteDetailsDTO.authToken
+                    it[headers] = updateWebsiteDetailsDTO.headers
+                    it[maxRetries] = updateWebsiteDetailsDTO.maxRetries
+                    it[updatedAt] = currentTime
                 }
                 RecordCreationErrorHandler.Success(
                     successMessage = "Website updated successfully",
